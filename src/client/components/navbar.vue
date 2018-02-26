cc28c41b259774ec66a44adf4da0cd94f8317927
yussan <yussandeveloper@gmail.com>
2018-02-26 21:26:21 +0700

    style(navbar): added search handler in navbar

--- src/client/components/navbar.vue
+++ src/client/components/navbar.vue
@@ -1,27 +1,52 @@
 <template lang="pug">
   nav 
     .container-medium 
-      ul.navbar 
+      ul.navbar(v-if='search === false')
         li 
-          router-link(to='/tag/android') ANDROID
+          router-link(to='/tag/android') MOBILE
         li
-          router-link(to='/tag/android') iOS
+          router-link(to='/tag/android') DESKTOP
         li
           router-link(to='/tag/web') WEB
         li
-          router-link(to='/tag/web') WINDOWS
-        li
-          router-link(to='/tag/web') MACOS
-        li
-          router-link(to='/tag/web') CLOUD
-        li
           router-link(to='/tag/web') DEVELOPER
+        li(style='float:right;margin:0;margin-top:-.4em;') 
+          a.icono-search(href='javascript:;'  v-on:click='toggleSearch' style='-webkit-transform: scale(.8) rotate(40deg);-ms-transform: scale(.8) rotate(40deg);transform: scale(.8) rotate(40deg);')
+      ul.search(v-if='search === true') 
+        input#search-input(type='text' v-model='search_text' v-on:keydown='handleChangeSearch' placeholder='search here...')
+        a.icono-cross(href='javascript:;' v-on:click='toggleSearch' style='position:absolute;margin-top:.5em;transform: translateX(-2em) rotate(45deg);')
 </template>
 
 <script lang="ts">
   import Vue from 'vue'
-  export default Vue.extend({
+  import { router } from '../index'
 
+  export default Vue.extend({
+    name: 'navbar',
+    data() {
+      return {
+        search: false,
+        search_text: ''
+      }
+    },
+    methods: {
+      toggleSearch() {
+        this.search = !this.search
+        if(this.search === true)
+          setTimeout(() => {
+            (document.getElementById('search-input')).focus()
+          }, 300)
+        else 
+          this.search_text = ''
+      },
+
+      handleChangeSearch(e: Object={}) {
+        console.log(e.keyCode, this.search_text)
+        if(e.keyCode == 13 && this.search_text != '') {
+          router.push({path: `/search?q=${this.search_text}`})
+        }
+      }
+    }
   })
 </script>
 
@@ -41,9 +66,22 @@
           transition: all .3s ease
           color: $color-white-main
           text-decoration: none
-          font-size: 1.2em
+          font-size: 1em
           letter-spacing: 1.5px
           &:hover 
             color: $color-gray-dark
+
+    ul.search
+      margin: 0
+      padding: .25em 0
+      input[type='text']
+        &:focus
+          outline: none
+        width: 100%
+        background: transparent
+        border: none
+        padding: .8em 0
+        color: #FFF
+        font-size: 1em
 </style>
 
