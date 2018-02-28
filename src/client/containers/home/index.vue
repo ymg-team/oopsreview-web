9e5375a0334acecc2f1959f1304d302c98f0eac1
yussan <yussandeveloper@gmail.com>
2018-02-28 14:37:29 +0700

    docs(readme.md): added preview and some dev stacs link

--- src/client/containers/home/index.vue
+++ src/client/containers/home/index.vue
@@ -8,6 +8,8 @@
           box-post
         .col-4
           sidebar
+        .col-12(style='padding-bottom: 0')
+          buttonBig(button_type='blue' to='/posts' text='More Stories')
 </template>
 
 <script lang="ts">
@@ -16,11 +18,13 @@ import popular from '../../components/boxs/popular-posts.vue'
 import title from '../../components/cards/title.vue'
 import post from '../../components/boxs/post.vue'
 import sidebar from '../../components/sidebar.vue'
+import buttonBig from '../../components/form/button-big.vue'
 
 Vue.component('popular-box', popular)
 Vue.component('card-title', title)
 Vue.component('box-post', post)
 Vue.component('sidebar', sidebar)
+Vue.component('buttonBig', buttonBig)
 
 export default Vue.extend({
   name: 'home'
