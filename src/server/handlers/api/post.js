7a6444032c4461b2ef8822b67aeba8812d0c15cd
yussan <yussandeveloper@gmail.com>
2018-03-13 22:21:20 +0700

    stable mongodb connection

--- src/server/handlers/api/post.js
+++ src/server/handlers/api/post.js
@@ -3,7 +3,7 @@ import response from '../../modules/response'
 
 export default (req, res, next) => {
   mongo().then(db => {
-    db.collection('news').find().toArray((err, result) => {
+    db.collection('posts').find().toArray((err, result) => {
       if(err) res.send(500, response(500, 'something wrong with mongo'))
       res.send(200, result)
     })
--- src/server/modules/mongo.js
+++ src/server/modules/mongo.js
@@ -4,7 +4,6 @@ import debug from 'debug'
 const mongoclient = mongodb.MongoClient
 const debugMongo = debug('app:mongo')
 const url = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}`
-console.log(url)
 export default () => {
   return new Promise((resolve, reject) => {
     mongodb.MongoClient.connect(url, (err, client) => {
