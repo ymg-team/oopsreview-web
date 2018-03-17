beac56a12ac001eae295abdf6936861571dd0ece
yussan <yussandeveloper@gmail.com>
2018-03-17 23:34:18 +0700

    ready post endpoint

--- src/server/handlers/api/post.js
+++ src/server/handlers/api/post.js
@@ -1,11 +1,38 @@
 import mongo from '../../modules/mongo'
 import response from '../../modules/response'
+import { DB_DEFAULT_LIMIT, DB_DEFAULT_PAGE } from '../../const'
 
-export default (req, res, next) => {
+import postTransformer from '../../transformers/post'
+import userTransformer from '../../transformers/user'
+
+export default (req, res) => {
+  const { page, limit } = req.query
   mongo().then(db => {
-    db.collection('posts').find().toArray((err, result) => {
-      if(err) res.send(500, response(500, 'something wrong with mongo'))
-      res.send(200, result)
+    db.collection('posts').aggregate([
+        {
+            $lookup: {
+                from: 'users',
+                localField: 'user_id',
+                foreignField: '_id',
+                as: 'author'
+            }
+        }
+    ]).skip(page || 0).limit(limit || DB_DEFAULT_LIMIT)
+      .toArray((err, result) => {
+          // error from database
+          if(err) {
+              console.log(err)
+              return res.send(500, response(500, 'something wrong with mongo'))
+          }
+
+          // transform data
+          result.map(n => {
+              n = postTransformer(n)
+              n.author = userTransformer(n.author[0])
+          })
+
+          // success
+          return res.send(200, response(200, 'success', { result }))
     })
   })
 }
--- src/server/modules/response.js
+++ src/server/modules/response.js
@@ -2,6 +2,11 @@ export default (code = 500, message = 'something wrong', data = {}) => {
   let response = {}
   
   switch(code) {
+    case 200 :
+      response = data
+      response.code = code
+      break
+
     default :
       response = {
         code,
