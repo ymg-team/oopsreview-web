4cbe02ff3db96de049effc8b1252222fd3f5d01a
yussan <yussandeveloper@gmail.com>
2018-07-14 13:13:16 +0700

    feat(post list): added search in post list

--- src/client/containers/post/index.vue
+++ src/client/containers/post/index.vue
@@ -43,6 +43,7 @@ export default Vue.extend({
 
   data() {
     let { tag_name } = this
+    let { q } = this.$route.query
     let title = "Posts",
       filter = "archived",
       subtitle = tag_name
@@ -52,6 +53,12 @@ export default Vue.extend({
     if (tag_name) {
       title = `Post by tag "${tag_name}"`
       filter = `archived_${tag_name}`
+    }
+
+    if(q) {
+      title = `Searching "${q}"`
+      filter = `archived_search_${q}`
+      subtitle = `Searching post by keyword "${q}"`
     }
 
     // initial data
@@ -90,14 +97,14 @@ export default Vue.extend({
 
   watch: {
     tag_name() {
-      const {tag_name} = this
-
+      const { tag_name } = this
+      
       let filter = "archived",
-      title = "posts",
-      subtitle = ""
+        title = "posts",
+        subtitle = ""
 
       if (tag_name) {
-        filter = `archived_${tag_name}`
+        filter = `archived_search_${tag_name}`
         title = `posts by tag "${tag_name}"`
         subtitle = `Find all available post posted with tag "${tag_name}"`
         this.$store.dispatch(TYPES.GET_TAG, tag_name)
@@ -110,6 +117,22 @@ export default Vue.extend({
       // fetch new data if not available in store
       const params = this.generateParams()
       if (!this.post[filter]) this.$store.dispatch(TYPES.GET_POSTS, params)
+    },
+
+    $route() {
+      const { q } = this.$route.query
+      if(q) {
+        const title = `Searching "${q}"`
+        const subtitle = `Searching post by keyword "${q}"`
+        const filter = `archived_${q}`
+        
+        this.title = title 
+        this.subtitle = subtitle
+        this.filter = filter 
+
+        const params = this.generateParams()
+        if (!this.post[filter]) this.$store.dispatch(TYPES.GET_POSTS, params)
+      }
     }
   },
 
@@ -132,8 +155,10 @@ export default Vue.extend({
     },
 
     generateParams() {
+      const { q } = this.$route.query
       let params: any = { filter: this.filter }
       if (this.tag_name) params.tag = this.tag_name
+      if (q) params.keyword = q
       return params
     }
   },
