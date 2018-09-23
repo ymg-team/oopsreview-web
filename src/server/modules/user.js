04fe76c2bd87646b274ac5c4ce43c637d099f8d8
yussan <yussandeveloper@gmail.com>
2018-09-23 19:45:50 +0700

    fix(seo tag): added seo tag for user posts and post detail
    Closes #56

---
+++ src/server/modules/user.js
@@ -0,0 +1,30 @@
+import mongo from "./mongo"
+import response from "../modules/response"
+import userTransformer from "../transformers/user"
+
+/**
+ * @description function to get user profile by username
+ * @param {string} req.params.username
+ */
+export const profileUser = (req, res, {username, callback}) => {
+  mongo().then(db => {
+    db.collection("users").aggregate([
+      {
+        $match: { username }
+      },
+    ]).toArray((err, result) => {
+      // error from database
+      if (err) {
+        console.log(err)
+        return res.send(500, response(500, "something wrong with mongo"))
+      }
+
+      if (result.length < 1) {
+        return res.send(200, response(204, "user not found"))
+      }
+
+      const user = userTransformer(result[0])
+      return callback(user)
+    })
+  }) 
+}
