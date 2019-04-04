79b4ba46fecc4480c70ed92fd3e6178189972da3
yussan <yussandeveloper@gmail.com>
2019-04-04 19:28:41 +0700

    fix(server side): added server side html for some pages
    BREAKING CHANGE: #72
    Closes #72

--- src/server/middlewares/frontMiddleware.js
+++ src/server/middlewares/frontMiddleware.js
@@ -6,14 +6,22 @@ import { truncate } from "string-manager/dist/modules/truncate"
 export const generateMetaPostList = (req, res, next) => {
   let title = "Post"
 
-  if(req.params.tag) title = `${title} by tag ${req.params.tag}`
+  if (req.params.tag) title = `${title} by tag ${req.params.tag}`
 
   req.meta = {
     title,
     desc: `${title} on Oopsreview`,
     url: `https://oopsreview.com/${req.originalUrl}`,
-    image: "https://res.cloudinary.com/dhjkktmal/image/upload/v1535163093/oopsreview/2018/default_post_image.png"
-  }   
+    image:
+      "https://res.cloudinary.com/dhjkktmal/image/upload/v1535163093/oopsreview/2018/default_post_image.png"
+  }
+
+  req.html = `
+    <div class="post-list">
+      <h1>${`${title} on Oopsreview`}</h1>
+      <div>Available ${title} on Oopsreview</div>
+    </div>
+  `
 
   return next()
 }
@@ -32,6 +40,25 @@ export const generateMetaPost = (req, res, next) => {
           url: `https://oopsreview.com/post/${req.params.title}`,
           image: json.image.original
         }
+
+        // generate html stringify
+        req.html = `
+          <div class="post-detail">
+            <h1>${json.title}</h1>
+            <figure>
+              <img src="${json.image.original}" alt="${json.title}">
+              <figcaption>${json.title}</figcaption>
+            </figure>
+            <article>
+              ${json.content}
+            </article>
+            <div class="post-detail_tag">
+              ${json.tags.map(n => {
+                return `<a href="/tag/${n}">${n}</a>`
+              })}
+            </div>
+          </div>
+        `
       }
 
       return next()
@@ -52,12 +79,23 @@ export const generateMetaUser = (req, res, next) => {
           url: `https://oopsreview.com/author/${username}`,
           image: json.avatar.original
         }
+
+        req.html = `
+          <div className="author">
+            <h1>Posted by ${username}</h1>
+            <figure>
+              <img src="${json.avatar.original}" alt="${username}" />
+              <figcaption>${username}</figcaption>
+            <figure>
+          </div>
+        `
       } else {
         req.meta = {
           title: "User Not Found",
           desc: "User Not Found",
           url: `https://oopsreview.com/author/${username}`
         }
+        
       }
 
       return next()
