f058bcf3b4cb79ff7c1c7dede684092421f8ba0a
Yusuf Akhsan Hidayat <yussan@Local-Computer.local>
2019-04-26 20:24:52 +0700

    create new mongodump

--- src/server/modules/mongo.js
+++ src/server/modules/mongo.js
@@ -1,10 +1,10 @@
-import mongodb from 'mongodb'
-import debug from 'debug'
+import mongodb from "mongodb"
+import debug from "debug"
 
-const {MONGO_USER, MONGO_PASSWORD, MONGO_DB, MONGO_HOST} = process.env
-const debugMongo = debug('app:mongo')
+const { MONGO_USER, MONGO_PASSWORD, MONGO_DB, MONGO_HOST } = process.env
+const debugMongo = debug("app:mongo")
 let url = ""
-if(MONGO_USER && MONGO_PASSWORD) {
+if (MONGO_USER && MONGO_PASSWORD) {
   url = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DB}`
 } else {
   url = `mongodb://${MONGO_HOST}`
@@ -12,11 +12,11 @@ if(MONGO_USER && MONGO_PASSWORD) {
 export default () => {
   return new Promise((resolve, reject) => {
     mongodb.MongoClient.connect(url, (err, client) => {
-      if(err) {
-        debugMongo('[error] to connect mongo')
-        debugMongo(err, 'mongo')
+      if (err) {
+        debugMongo("[error] to connect mongo")
+        debugMongo(err, "mongo")
       } else {
-        debugMongo('[success] connected mongo server')
+        debugMongo("[success] connected mongo server")
         const db = client.db(MONGO_DB)
         resolve(db)
       }
